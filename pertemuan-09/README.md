02654e59d6822c2dadbb361e91b1877098310301
Yohanes Setiawan Japriadi <ysetiawanj@atmaluhur.ac.id>
2025-11-25 11:23:17 +0700

    tangkap post dan session untuk form biodata

--- pertemuan-09/README.md
+++ pertemuan-09/README.md
@@ -5,4 +5,5 @@
   <li>Membuat form untuk biodata sederhana mahasiswa</li>
   <li>styling section biodata</li>
   <li>styling section biodata beres</li>
+  <li>tangkap post dan session untuk form biodata</li>
 </ol>
--- pertemuan-09/proses.php
+++ pertemuan-09/proses.php
@@ -6,5 +6,16 @@ $sespesan = $_POST["txtPesan"];
 $_SESSION["sesnama"] = $sesnama;
 $_SESSION["sesemail"] = $sesemail;
 $_SESSION["sespesan"] = $sespesan;
+
+$txtNim = $_POST["txtNim"];
+$txtNmLengkap = $_POST["txtNmLengkap"];
+$txtT4Lhr = $_POST["txtT4Lhr"];
+$txtTglLhr = $_POST["txtTglLhr"];
+$txtHobi = $_POST["txtHobi"];
+$txtPasangan = $_POST["txtPasangan"];
+$txtKerja = $_POST["txtKerja"];
+$txtNmOrtu = $_POST["txtNmOrtu"];
+$txtNmKakak = $_POST["txtNmKakak"];
+$txtNmAdik = $_POST["txtNmAdik"];
 header("location: index.php");
 ?>
